aa186e7d8d1dde133d4ec0af524a9a97b7bd0d7c
EvanAristy <evanjaristy@gmail.com>
2022-03-03 14:32:36 -0500

    Did my routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Route, Routes } from 'react-router-dom'
 // components
 import Nav from './components/Nav';
+// pages
+import Info from './pages/Info';
+import SignIn from './pages/SignIn';
+import Messages from './pages/Messages';
 // css
 import './App.css';
 
@@ -7,7 +12,11 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      
+      <Routes>
+        <Route path='info' element={<Info />} />
+        <Route path='messages' element={<Messages />} />
+        <Route path='signin' element={<SignIn />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import { Link } from "react-router-dom"
 
 const Nav = () => {
   return (
 
-    <div class="ui inverted segment">
-      <div class="ui inverted secondary pointing menu">
-        <a class="olive item">Home</a>
-        <a class="blue item">Messages</a>
-        <a class="orange item">Info</a>
+    <div className="ui inverted segment">
+      <div className="ui inverted secondary pointing menu">
+        <Link className="olive item" to='/'>Home</Link>
+        <Link className="blue item" to='/messages'>Messages</Link>
+        <Link className="orange item" to='/info'>Info</Link>
         <div className="right menu">
-            <a class="violet item active">Sign Up/In</a>
+            <Link className="violet item active" to='/signin'>Sign Up/Log In</Link>
         </div>
       </div>
     </div>
